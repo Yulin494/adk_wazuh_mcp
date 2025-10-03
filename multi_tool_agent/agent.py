7f7e0b06f7461b3f4eab22e3e999dc007d627e21
yulin494 <0928260126max@gmail.com>
2025-10-03 11:21:05 +0800

    add cortex mcp server

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -17,15 +17,31 @@ wazuh_toolset = MCPToolset(
     ),
 )
 
+cortex_toolset = MCPToolset(
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command='/Users/imac/.cargo/bin/mcp-server-cortex',  # 請將此路徑替換為你的 mcp-server-cortex 可執行檔路徑
+            args=[],
+            env={
+                "CORTEX_ENDPOINT": "Your Cortex Endpoint",
+                "CORTEX_API_KEY": "Your Cortex API Key",
+                "RUST_LOG": "info"
+            },
+        ),
+    ),
+)
+
 root_agent = LlmAgent(
     model='gemini-2.5-flash', 
     name='wazuh_agent', 
     instruction=(
-        "你是一個 Wazuh 資安助理。你的任務是使用提供的工具來回答關於 Wazuh 平台的查詢。"
+        "你是一個 Wazuh 結合 cortex 資安助理。你的任務是使用提供的工具來回答關於 Wazuh 平台的查詢並且使用cortex 分析儀分析可疑的ip或url。"
         "如果使用者說查詢最近幾筆的話，就limit在5筆以內。"
         "請根據使用者的問題，選擇最合適的工具來回覆準確的資訊，並且把得到的資訊彙整起來給使用者。"
+        "只把標題放大，並改成紅色字"
     ),
     tools=[
-        wazuh_toolset
+        wazuh_toolset,
+        cortex_toolset
     ],
 )
